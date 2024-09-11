e5d3e4228f8fb39766ba9513b2f377269fa2dd58
mohitsuratiya <167969274+mohitsuratiya@users.noreply.github.com>
2024-09-11 19:08:38 +0530

    fifth commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,21 +4,18 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchuser')
 
 
 const JWT_SECRET = 'mohitisagoodB$oy';
 
 // route 1: user using a post api/auth/createuser
 
-router.get("/createuser", [
+router.post("/createuser", [
     body('name', 'enter the valid name').isLength({min: 3}),
     body('email', 'enter the valid email').isEmail(),
     body('password', 'password must be atleast 5 characters').isLength({min: 5}),
 ], async (req, res)=>{
-    // const errors = validationResult(req);
-    // if(!errors.isEmpty()){
-    //     return res.status(400).json({errors: errors.array});
-    // }
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -43,11 +40,10 @@ const salt = await bcrypt.genSalt(10);
     user:{
       id: user.id
     }
-  }
+  };
    
- const authtoken =   jwt.sign(data, JWT_SECRET)
-
-    res.json({authtoken})
+ const authtoken =   jwt.sign(data, JWT_SECRET);
+    res.json({authtoken});
 
 
 }catch(error){
@@ -60,7 +56,7 @@ const salt = await bcrypt.genSalt(10);
 
 //route 2: user login
 
-router.get("/login", [
+router.post("/login", [
   body('email', 'enter the valid email').isEmail(),
   body('password', 'password cannot be blank').exists(),
 ], async (req, res)=>{
@@ -92,11 +88,29 @@ try {
     res.json({authtoken})
 
 } catch(error){
-  console.log(error.message);
+  console.error(error.message);
   res.status(500).send(" internal server error")
 }
 
 
 })
+
+
+
+//route 3: get login user detalis api/auth/getuser
+
+router.post('/getuser', fetchuser, async (req, res) =>{
+  try {
+   userId = req.user.id;
+    const user = await User.findById(userId).select("-password")
+    res.send(user)
+  }
+   catch(error){
+    console.error(error.message);
+  res.status(500).send(" internal server error")
+  }
+
+
+} )
 
 module.exports = router
